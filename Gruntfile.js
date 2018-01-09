ad079bb249ce467206e05ab11404c120217b7d38
ErickMarin <emarin@anzen.com.mx>
2018-01-09 16:11:06 -0600

    chore: Add config files to run develop

---
+++ Gruntfile.js
@@ -0,0 +1,54 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    clean: {
+      build: ['tmp/']
+    },
+    connect: {
+      server: {
+        options: {
+          port: 8888,
+          livereload: 35729,
+          hostname: 'localhost',
+          path: 'www-root',
+          options: {
+            index: 'index.html'
+          },
+          open: false,
+          reload: true
+        }
+      }
+    },
+    wiredep: {
+      task: {
+        src: 'index.html'
+      }
+    },
+    copy: {
+      main: {
+        files: [{
+          expand: true,
+          cwd: 'src/scripts',
+          src: '*',
+          dest: 'tmp/scripts/'
+        }]
+      }
+    },
+    watch: {
+      livereload: {
+        options: {livereload: true, spawn: true},
+        tasks: ['clean', 'wiredep', 'copy'],
+        files: 'src/**/*',
+        reload: true
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-wiredep');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('default', ['clean', 'wiredep', 'copy', 'connect:server', 'watch']);
+}
