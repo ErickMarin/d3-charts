6906f120930d1e24c9f9878ac435cf7f94c10559
ErickMarin <emarin@anzen.com.mx>
2018-01-19 11:00:30 -0600

    feat:art: Add degree and format in yAxis

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -37,7 +37,7 @@ const data = [{
     "precioApertura": 230,
     "precioUltimo": 228.9,
     "precioMinimo": 228,
-    "precioMaximo": 231,
+    "precioMaximo": 31,
     "volumen": 10
 },
 {
@@ -61,11 +61,11 @@ const data = [{
     "precioApertura": 230,
     "precioUltimo": 230,
     "precioMinimo": 228,
-    "precioMaximo": 231,
+    "precioMaximo": 31,
     "volumen": 10
 }];
 
-const margin = {top:20, right:0, bottom:20, left:40};
+const margin = {top:20, right:20, bottom:20, left:20};
 const height = 300 - margin.top - margin.bottom;
 const width = 600 - margin.right - margin.left;
 
@@ -78,6 +78,8 @@ const svg = d3.select('svg')
 
 const es_MX = d3.timeFormatLocale({decimal: ".",thousands: ",",grouping: [3],currency: ["$", ""],dateTime: "%A, %e de %B de %Y, %X",date: "%d/%m/%Y",time: "%H:%M:%S",periods: ["AM", "PM"],days: ["domingo", "lunes", "martes", "miércoles", "jueves", "viernes", "sábado"],shortDays: ["dom", "lun", "mar", "mié", "jue", "vie", "sáb"],months: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"],shortMonths: ["Ene", "Feb", "Mar", "Abr", "May", "Jun", "Jul", "Ago", "Sep", "Oct", "Nov", "Dic"]});
 
+const parseTime = d3.timeParse('%Y-%m-%j %H:%M:%S');
+
 const x = d3.scaleTime()
   .range([0, width]);
 
@@ -85,24 +87,21 @@ const y = d3.scaleLinear()
   .range([height, 1]);
 
 const xAxis = d3.axisBottom()
-  .scale(x)
-  .ticks(d3.timeMonths)
-  .tickSize(15, 0)
-  .tickFormat(es_MX.timeFormat('%b %d'));
+  .scale(x);
 
-const yAxis = d3.axisRight(y)
+const yAxis = d3.axisLeft(y)
   .ticks(5)
-  .tickSize(-width, 0);
+  .tickSize(0, -width);
 
 const area = d3.area()
-  .x(function(d) { return x(d.tiempo); })
+  .x(function(d) { return x(parseTime(d.fecha)); })
   .y1(function(d) { return y(d.precioMaximo); })
   .y0(y(0))
   .curve(d3.curveNatural);
 
 
 /* Begin the iterate with data */
-x.domain(d3.extent(data, function(d) { return d.tiempo; }));
+x.domain(d3.extent(data, function(d) { return parseTime(d.fecha); }));
 y.domain([0, d3.max(data, function(d) { return d.precioMaximo; })]);
 
 const dataNest = d3.nest()
@@ -111,6 +110,7 @@ const dataNest = d3.nest()
 
 svg.append('path')
   .attr('class', 'area')
+  .attr('transform', 'translate(-20, 20)')
   .attr('d', area);
 
 svg.append('g')
@@ -125,10 +125,10 @@ svg.append('g')
   .attr('transform', 'translate(' + width + ',0)')
   .call(yAxis);
 
-svg.selectAll('.dot')
+/*svg.selectAll('.dot')
   .data(data.filter(function(d) { return d; }))
   .enter().append('circle')
     .attr('class', 'dot')
     .attr('cx', area.x())
-    .attr('cy', area.y())
-    .attr('r', 3.5);
+    .attr('cy', area.y1())
+    .attr('r', 3.5);*/
